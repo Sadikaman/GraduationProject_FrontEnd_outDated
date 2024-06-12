65266b854f2af0841e8a5872844a06f7079f68d5
Nurhussen <nuryassinm132@gmail.com>
2024-06-12 13:47:08 -0500

    adding content to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,11 +5,38 @@ function About() {
     <>
     <section className='flex  flex-col'>
         <Nav />
-        <div className='flex justify-center'>
-<div className='bg-cover bg-no-repeat flex  justify-center items-center  w-[1000px] ' style={{backgroundImage:"url('ethiopiaAbout.jpeg')"}}>
-   <p className='pt-20 pb-20'>About us </p>
-</div>
-</div>
+          <div className='flex justify-center'>
+            <div className='flex justify-center w-[1500px] h-56 '>
+              <div className='bg-cover bg-no-repeat bg-center flex  justify-center items-center  w-full h-full rounded-xl ' style={{backgroundImage:"url('About.jpg')"}}>
+                <p className='text-white text-4xl font-medium'>About Us </p>
+              </div>
+            </div>
+          </div>
+
+{/* what we do  */}
+      <div className='flex justify-center w-full flex-col mt-20'>
+        
+        <div className='w-full flex justify-center'>
+        <div className='flex gap-10 w-[80%]'>
+          <img src="plain.jpeg" className='w-[1400px]' alt="" />
+          <h1 className='flex flex-col text-4xl font-bold text-[#2986FE] gap-3 text-center'>What We Do 
+            <p className='text-black text-2xl font-normal text-start'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem, cumque excepturi molestias totam alias veritatis, impedit sed possimus ipsamolestias totam alias veritatis, impedit sed possimus ipsa quas laborum pariatur! Neque ducimus sint odio, necessitatibus nemo officiis nam.</p>
+
+          <p className='text-black text-2xl font-normal text-start'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non ipsum perferendis sit eum repudiandae unde. Est nobis corporis possimus! Aperiam ab sed molestias totam alias veritatis, impedit sed possimus ipsa consectetur, obcaecati error doloribus quae soluta beatae quos.</p></h1>
+        </div>
+        </div>
+
+        <div className='w-full flex justify-center mt-20'>
+        <div className='flex gap-10 w-[80%]'>
+          <h1 className='flex flex-col text-4xl font-bold text-[#2986FE] gap-3 text-center'>What We Do 
+            <p className='text-black text-2xl font-normal text-start'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem, cumque excepturi molestias totam alias veritatis, impedit sed possimus ipsamolestias totam alias veritatis, impedit sed possimus ipsa quas laborum pariatur! Neque ducimus sint odio, necessitatibus nemo officiis nam.</p>
+
+          <p className='text-black text-2xl font-normal text-start'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non ipsum perferendis sit eum repudiandae unde. Est nobis corporis possimus! Aperiam ab sed molestias totam alias veritatis, impedit sed possimus ipsa consectetur, obcaecati error doloribus quae soluta beatae quos.</p></h1>
+          <img src="plain.jpeg" className='w-[1400px]' alt="" />
+        </div>
+        </div>
+        
+      </div>
     </section>
     </>
   )
